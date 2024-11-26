5530ef2872fdc2422068f1b7da8d30122a0e9643
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-26 09:51:55 -0500

    updates

---
+++ test_sum_2.py
@@ -0,0 +1,12 @@
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    test_sum_tuple()
+    print("Everything passed")
+    
+# This would not run because the test_sum_tuple() function is not defined in the test_sum_2.py file.
