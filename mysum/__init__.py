b1fc5beed61835157f596ca777953bdea78fcc21
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-26 17:25:38 -0500

    Finalize ipynb

--- mysum/__init__.py
+++ mysum/__init__.py
@@ -6,3 +6,15 @@ def sum(arg):
 
 target = __import__("my_sum.py")
 sum = target.sum
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6" # This is saying that the sum of 1, 2, 3 should be 6 or whatever inside the function should be equal to 6
+
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 5, "Should be 5" # This is saying that the sum of 1, 2, 2 should be 5 or whatever inside the function should be equal to 5
+    test_sum()
+    test_sum_tuple()
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
